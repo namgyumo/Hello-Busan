d7b32d1e593231fef3c4cab848f62c8041761ec4
Nam-Gyumo <n.gyumo13@gmail.com>
2026-03-26 13:18:55 +0900

    Create __init__.py

---
+++ backend/models/__init__.py
@@ -0,0 +1,6 @@
+"""
+데이터 모델 패키지
+"""
+from backend.models.spot import SpotResponse, SpotDetail, SpotListResponse
+from backend.models.comfort import ComfortResponse, ComfortDashboard
+from backend.models.common import PaginatedResponse, ErrorResponse
